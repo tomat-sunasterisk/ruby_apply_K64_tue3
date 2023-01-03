99987bea73b6a95e1c52422710c59d9c053351d0
Replit user <>
2023-01-03 08:29:54 +0000

    kadai2

---
+++ kadai2.rb
@@ -0,0 +1,17 @@
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+  def initialize(name, price)
+    self.name = name
+    self.price = price
+  end
+end
+
+menu1 = Menu.new('Phở', 30_000)
+menu2 = Menu.new('Bún chả', 40_000)
+menu3 = Menu.new('Bánh mì', 20_000)
+menus = [menu1, menu2, menu3]
+
+menus.each do |_n|
+  puts "#{name} #{total_price}vnd"
+end
