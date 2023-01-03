cb562ad57e761129d504c3c8f9fabb656688b8cb
Replit user <>
2023-01-03 09:49:09 +0000

    kadai1

--- kadai1.rb
+++ kadai1.rb
@@ -2,10 +2,12 @@ class Menu
  attr_accessor :name
  attr_accessor :price
   # get_total_priceメソッドを定義してください
-
-
-
-  
+  def get_total_price(count)
+    if count < 3
+      total_price = self.price * count
+    else 
+      total_price = self.price*count - 10000
+    end
    return "#{self.name} #{total_price}vnd"
  end
 end
@@ -15,4 +17,4 @@ menu1.name = "Phở"
 menu1.price = 30000
 
 # menu1に対してget_total_priceメソッドを呼び出してください
-
+puts menu1.get_total_price(4)
