675b8a402c6d75b052962f9ba6f2169b58e1bfae
Replit user <>
2023-01-03 08:52:56 +0000

    kadai1234

--- kadai1.rb
+++ kadai1.rb
@@ -1,18 +1,24 @@
 class Menu
- attr_accessor :name
- attr_accessor :price
+  attr_accessor :name
+  attr_accessor :price
   # get_total_priceメソッドを定義してください
+  def get_total_price(count)
+    total_price = price * count
 
-
-
-  
-   return "#{self.name} #{total_price}vnd"
- end
+    if count >= 3
+      total_price -= 10_000
+      "#{name} #{total_price}vnd"
+    else
+      "#{name} #{total_price}vnd"
+    end
+  end
 end
 
 menu1 = Menu.new
-menu1.name = "Phở"
-menu1.price = 30000
+menu1.name = 'Phở'
+menu1.price = 30_000
 
 # menu1に対してget_total_priceメソッドを呼び出してください
-
+puts('input count: ')
+count = gets.to_i
+puts menu1.get_total_price(count)
