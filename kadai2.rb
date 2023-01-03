2e6960f7b3dd58fe3a53a2732d57f4f6aa4adddc
tungnguyensnk <tung.nd194714@sis.hust.edu.vn>
2023-01-03 16:36:51 +0700

    kadai1234

---
+++ kadai2.rb
@@ -0,0 +1,25 @@
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+
+  def initialize(name:, price:)
+    self.name = name
+    self.price = price
+  end
+end
+
+class Drink < Menu
+  def initialize(name:, price:)
+    super(name: name, price: price)
+  end
+end
+
+menu1 = Menu.new(name: "Phở", price: 30000)
+menu2 = Menu.new(name: "Bún chả", price: 40000)
+menu3 = Menu.new(name: "Bánh mì", price: 20000)
+
+menus = [] << menu1 << menu2 << menu3
+
+menus.each do |menu|
+  puts "#{menu.name} #{menu.price}vnd"
+end
