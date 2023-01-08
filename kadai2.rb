086a26c9382006d4cd2bb9035c85fc5d05c8cae8
kien2572001 <kien2572001.ntk@gmail.com>
2023-01-08 23:06:23 +0700

    feat: add 4 kadai

--- kadai2.rb
+++ kadai2.rb
@@ -11,10 +11,26 @@ class Menu
  end
 end
 
-
-
+# create array of menu
+menu = []
 menu1 = Menu.new
 menu1.name = "Pho"
 menu1.price = 30000
+menu.push(menu1)
 
-puts "#{menu1.get_total_price(4)}"
+menu2 = Menu.new
+menu2.name = "Bun cha"
+menu2.price = 40000
+menu.push(menu2)
+
+menu3 = Menu.new
+menu3.name = "Banh mi"
+menu3.price = 20000
+menu.push(menu3)
+
+# print menu
+puts "Menu"
+menu.each do |item|
+  puts "#{item.name} #{item.price}vnd"
+end
+
---
+++ kadai3.rb
@@ -0,0 +1,44 @@
+class Menu
+    attr_accessor :name
+    attr_accessor :price
+     # get_total_priceメソッドを定義してください
+     def get_total_price(count)
+       total_price = self.price * count
+       if count >= 3
+         total_price -= 10000
+       end  
+      return "#{self.name} #{total_price}vnd"
+    end
+   end
+
+   class Drink < Menu
+    attr_accessor :size
+   end
+   
+   # create array of menu
+   menu = []
+   menu1 = Menu.new
+   menu1.name = "Pho"
+   menu1.price = 30000
+   menu.push(menu1)
+   
+   menu2 = Menu.new
+   menu2.name = "Bun cha"
+   menu2.price = 40000
+   menu.push(menu2)
+   
+   menu3 = Menu.new
+   menu3.name = "Banh mi"
+   menu3.price = 20000
+   menu.push(menu3)
+   
+    drink1 = Drink.new
+    drink1.name = "Tra
+
+   # print menu
+   puts "Menu"
+   menu.each do |item|
+     puts "#{item.name} #{item.price}vnd"
+   end
+   
+   
